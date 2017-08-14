e18d77a228603585249e813c96d758799aa1d005
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-08-14 18:27:09 +0200

    Addesd gulp task for sass and include normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,15 @@ var browserSync = require("browser-sync").create();
 // for html
 var htmlmin = require("gulp-htmlmin");
 
+// for css
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+
 // default task for development
-gulp.task("default", ["html"], function(){
+gulp.task("default", ["html", "sass"], function(){
     // launch develop local server
     browserSync.init({
         server: "dist/"
@@ -14,6 +21,8 @@ gulp.task("default", ["html"], function(){
     // watch html files to reload browser
     gulp.watch(["src/*.html", "src/**/*.html"], ["html"]);
 
+    // watch styles folder to compile sass files
+    gulp.watch(["src/styles/*.scss", "src/styles/**/*.scss"], ["sass"]);
 });
 
 // compile html files
@@ -24,5 +33,27 @@ gulp.task("html", function(){
         // copy to dist folder
         .pipe(gulp.dest("dist"))
         /// and reload browsers
+        .pipe(browserSync.stream());
+});
+
+
+// compile css styles from sass files
+gulp.task("sass", function(){
+    gulp.src("src/styles/*.scss")
+        // capture sourcemaps
+        .pipe(sourcemaps.init())
+        // compile sass
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postcss([
+            // add prefixes to old browsers compatibility
+            autoprefixer(),
+            // compress compiled css
+            cssnano()
+        ]))
+        // save sourcemaps in css folder
+        .pipe(sourcemaps.write("./"))
+        // copy to dist folder
+        .pipe(gulp.dest("dist/css/"))
+        // and reload browsers
         .pipe(browserSync.stream());
 });
