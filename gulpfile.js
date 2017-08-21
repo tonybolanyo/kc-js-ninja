a7df6fab14dce66ac49f111620e4019d60ac0cb4
Antonio G. Bolaño <angonzalez@odec.es>
2017-08-21 12:15:43 +0200

    Rules for stylelint extracted to .stylelintrc file

--- gulpfile.js
+++ gulpfile.js
@@ -23,34 +23,6 @@ var cssnano = require("cssnano");
 var uncss = require('gulp-uncss');
 var stylelint = require('stylelint');
 
-var stylelintConfig = {
-    "rules": {
-        "block-no-empty": true,
-        "color-no-invalid-hex": true,
-        "declaration-colon-space-after": "always",
-        "declaration-colon-space-before": "never",
-        "function-comma-space-after": "always",
-        "function-url-quotes": "always",
-        "media-feature-colon-space-after": "always",
-        "media-feature-colon-space-before": "never",
-        "media-feature-name-no-vendor-prefix": true,
-        "max-empty-lines": 5,
-        "number-leading-zero": "never",
-        "number-no-trailing-zeros": true,
-        "property-no-vendor-prefix": true,
-        "declaration-block-no-duplicate-properties": true,
-        "block-opening-brace-newline-after": "always",
-        "block-closing-brace-newline-before": "always",
-        "declaration-block-trailing-semicolon": "always",
-        "selector-list-comma-space-before": "never",
-        "selector-list-comma-newline-after": "always-multi-line",
-        //"selector-no-id": true,
-        "string-quotes": "double",
-        "value-no-vendor-prefix": true,
-        "max-nesting-depth": [3, { ignore: ["blockless-at-rules"] }]
-    }
-};
-
 // for JavaScript
 var tap = require("gulp-tap");
 var browserify = require("browserify");
@@ -126,7 +98,7 @@ gulp.task("sass:lint", function(){
     gulp.src(["src/styles/*.scss", "src/styles/**/*.scss"])
         .pipe(postcss([
             // lint style files
-            stylelint(stylelintConfig)
+            stylelint()
         ]))
 });
 
