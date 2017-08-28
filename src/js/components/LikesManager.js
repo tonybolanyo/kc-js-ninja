9fbe6db61844abe780578a7a3105afafb0990a03
Antonio G. Bolaño <angonzalez@odec.es>
2017-08-28 11:18:23 +0200

    Refactoring LikesManager

--- src/js/components/LikesManager.js
+++ src/js/components/LikesManager.js
@@ -1,5 +1,7 @@
 import $ from "jquery";
 
+// this must be taken from session
+// meanwhile, I use constant value to simulate user is logged in
 const currentUserId = 3;
 
 export default class LikesManager {
@@ -9,33 +11,38 @@ export default class LikesManager {
     }
 
     init() {
-        console.log("Init LikesManager");
         this.element.on("click", (event) => {
             const obj = $(event.currentTarget);
-            const articleId = obj.data("article-id");
-            const icon = obj.find(".icon");
-            icon.toggleClass("icon-liked").toggleClass("icon-like");
-            this.likesService.toggleArticleLike(articleId, currentUserId);
-            this.updateLikesCounter(obj, articleId);
+            this.toggleLike(obj);
             return false;
         });
         this.element.each((index) => {
             const obj = $(this.element[index]);
             const id = obj.data("article-id");
-            const liked = this.likesService.isArticleLiked(id, currentUserId);
-            const icon = obj.find('.icon');
-            console.log(icon);
-            if (liked) {
-                icon.addClass('icon-liked').removeClass('icon-like');
-            }
-            console.log("id: " + id, "liked: " + liked);
+            this.updateLikeIconStatus(obj, id);
             this.updateLikesCounter(obj, id);
         });
     }
 
+    toggleLike(element) {
+        const articleId = element.data("article-id");
+        this.likesService.toggleArticleLike(articleId, currentUserId);
+        this.updateLikesCounter(obj, articleId);
+        this.updateLikeIconStatus(obj, articleId);
+    }
+
     updateLikesCounter(element, articleId) {
-        console.log("Get article likes", articleId)
         const likes = this.likesService.getArticleLikesCount(articleId);
         $(element).find(".article-likes-count").html(likes)
+    }
+
+    updateLikeIconStatus(element, articleId) {
+        const liked = this.likesService.isArticleLiked(articleId, currentUserId);
+        const icon = element.find(".icon");
+        if (liked) {
+            icon.addClass("icon-liked").removeClass("icon-like");
+        } else {
+            icon.addClass("icon-like").removeClass("icon-liked");
+        }
     }
 }
